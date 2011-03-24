20e10c4f00324604be7312b0f8ccda1ee78e0584
manish <manish.das@sprout-technology.com>
2011-03-24 08:59:19 +0545

    New table created - author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110323100040) do
+ActiveRecord::Schema.define(:version => 20110324031138) do
+
+  create_table "authors", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "sex"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
